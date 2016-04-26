15f3d3df82175c5e5134d6baecf1ef05eb4f6bcf
mcmdhr <623742750@qq.com>
2016-04-26 19:07:01 +0800

    Add announcement module.

---
+++ announcement/serializers.py
@@ -0,0 +1,37 @@
+#=============================================================================
+# Author       : MaxMao
+# Email        : 623742750@qq.com
+# Last modified: 2016-04-26 18:57
+# Filename     : serializers.py
+# Description :
+#=============================================================================
+# coding=utf-8
+from rest_framework import serializers
+
+from account.models import User
+from .models import Announcement
+
+
+class CreateAnnouncementSerializer(serializers.Serializer):
+    title = serializers.CharField(max_length=50)
+    content = serializers.CharField(max_length=10000)
+
+
+class AnnouncementSerializer(serializers.ModelSerializer):
+
+    class UserSerializer(serializers.ModelSerializer):
+        class Meta:
+            model = User
+            fields = ["username"]
+
+    created_by = UserSerializer()
+
+    class Meta:
+        model = Announcement
+
+
+class EditAnnouncementSerializer(serializers.Serializer):
+    id = serializers.IntegerField()
+    title = serializers.CharField(max_length=50)
+    content = serializers.CharField(max_length=10000)
+    visible = serializers.BooleanField()
