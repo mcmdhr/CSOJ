6762b4b016a029a9e120b4cb1a30b40bb94454a3
mcmdhr <623742750@qq.com>
2016-04-27 15:10:56 +0800

    Add submission module.

---
+++ submission/tasks.py
@@ -0,0 +1,16 @@
+#=============================================================================
+# Author       : MaxMao
+# Email        : 623742750@qq.com
+# Last modified: 2016-04-27 14:56
+# Filename     : tasks.py
+# Description :
+#=============================================================================
+# coding=utf-8
+from __future__ import absolute_import
+from celery import shared_task
+from judge_dispatcher.tasks import JudgeDispatcher
+
+
+@shared_task
+def _judge(submission, time_limit, memory_limit, test_case_id):
+    JudgeDispatcher(submission, time_limit, memory_limit, test_case_id).judge()
