3579f9be9c34a1ad967781a3a2b8fdeba85b19d2
mcmdhr <623742750@qq.com>
2016-04-27 15:10:32 +0800

    Add problem module.

---
+++ problem/admin.py
@@ -0,0 +1,10 @@
+#=============================================================================
+# Author       : MaxMao
+# Email        : 623742750@qq.com
+# Last modified: 2016-04-27 14:20
+# Filename     : admin.py
+# Description :
+#=============================================================================
+from django.contrib import admin
+
+# Register your models here.
