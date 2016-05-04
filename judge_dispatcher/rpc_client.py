13bdb6023d5be320f201e5a7aaf809a889ce33da
mcmdhr <623742750@qq.com>
2016-05-04 14:34:30 +0800

    Add judge_dispatcher module.

---
+++ judge_dispatcher/rpc_client.py
@@ -0,0 +1,31 @@
+#=============================================================================
+# Author       : MaxMao
+# Email        : 623742750@qq.com
+# Last modified: 2016-05-04 13:50
+# Filename     : rpc_client.py
+# Description :
+#=============================================================================
+# coding=utf-8
+import xmlrpclib
+import httplib
+
+
+class TimeoutHTTPConnection(httplib.HTTPConnection):
+    def __init__(self, host, timeout=10):
+        httplib.HTTPConnection.__init__(self, host, timeout=timeout)
+
+
+class TimeoutTransport(xmlrpclib.Transport):
+    def __init__(self, timeout=10, *args, **kwargs):
+        xmlrpclib.Transport.__init__(self, *args, **kwargs)
+        self.timeout = timeout
+
+    def make_connection(self, host):
+        conn = TimeoutHTTPConnection(host, self.timeout)
+        return conn
+
+
+class TimeoutServerProxy(xmlrpclib.ServerProxy):
+    def __init__(self, uri, timeout=10, *args, **kwargs):
+        kwargs['transport'] = TimeoutTransport(timeout=timeout, use_datetime=kwargs.get('use_datetime', 0))
+        xmlrpclib.ServerProxy.__init__(self, uri, *args, **kwargs)
---
+++ judge_dispatcher/serializers.py
@@ -0,0 +1,36 @@
+#=============================================================================
+# Author       : MaxMao
+# Email        : 623742750@qq.com
+# Last modified: 2016-05-04 13:50
+# Filename     : serializers.py
+# Description :
+#=============================================================================
+# coding=utf-8
+import json
+from rest_framework import serializers
+from .models import JudgeServer
+
+
+class CreateJudgesSerializer(serializers.Serializer):
+    name = serializers.CharField(max_length=30)
+    ip = serializers.IPAddressField()
+    port = serializers.IntegerField()
+    # 这个服务器最大可能运行的判题实例数量
+    max_instance_number = serializers.IntegerField()
+    token = serializers.CharField(max_length=30)
+
+
+class EditJudgesSerializer(serializers.Serializer):
+    id = serializers.IntegerField()
+    name = serializers.CharField(max_length=30)
+    ip = serializers.IPAddressField()
+    port = serializers.IntegerField()
+    # 这个服务器最大可能运行的判题实例数量
+    max_instance_number = serializers.IntegerField()
+    token = serializers.CharField(max_length=30)
+    status = serializers.BooleanField()
+
+
+class JudgesSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = JudgeServer
