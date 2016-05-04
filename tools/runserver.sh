e6d22645547b862d9fdce86ff1143bd9b334ed1f
mcmdhr <623742750@qq.com>
2016-05-04 14:34:49 +0800

    Add tools.

---
+++ tools/runserver.sh
@@ -0,0 +1,9 @@
+#=============================================================================
+# Author       : MaxMao
+# Email        : 623742750@qq.com
+# Last modified: 2016-05-04 14:07
+# Filename     : runserver.sh
+# Description :
+#=============================================================================
+#!/usr/bin/env bash
+python manage.py runserver
---
+++ tools/runtest.sh
@@ -0,0 +1,14 @@
+#=============================================================================
+# Author       : MaxMao
+# Email        : 623742750@qq.com
+# Last modified: 2016-05-04 14:09
+# Filename     : runtest.sh
+# Description :
+#=============================================================================
+#!/usr/bin/env bash
+coverage run --source='.' manage.py test
+test_result=$?
+if [ "$test_result" -eq 0 ];then
+    coverage html
+    open htmlcov/index.html
+fi
