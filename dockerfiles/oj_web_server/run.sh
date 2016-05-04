51a6e009b15d81edbee7ccee9c4b583e5efcd416
mcmdhr <623742750@qq.com>
2016-05-04 14:38:33 +0800

    Minor update.

--- dockerfiles/oj_web_server/run.sh
+++ dockerfiles/oj_web_server/run.sh
@@ -1,7 +1,7 @@
-#!/user/bin/env bash
-if [ ! -f "/code/oj/custom_settings.py"]; then
-    cp /code/oj/custom_settings.example.py /code/oj/custom_settings.py
-    echo "SECRET_KEY=\"`cat /dev/urandom | head -1 | md5sum | head -c 32`\"" >> /code/oj/custom_settings.py
+#!/usr/bin/env bash
+if [ ! -f "/code/oj/custom_settings.py" ]; then
+ cp /code/oj/custom_settings.example.py /code/oj/custom_settings.py
+ echo "SECRET_KEY=\"`cat /dev/urandom | head -1 | md5sum | head -c 32`\"" >> /code/oj/custom_settings.py
 fi
 find /code -name "*.pyc" -delete
 python -m compileall /code
