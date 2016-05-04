1f488b616503499b94a7819922460718dbf271a2
mcmdhr <623742750@qq.com>
2016-05-04 14:35:27 +0800

    Add manage.

---
+++ manage.py
@@ -0,0 +1,17 @@
+#=============================================================================
+# Author       : MaxMao
+# Email        : 623742750@qq.com
+# Last modified: 2016-04-27 09:39
+# Filename     : manage.py
+# Description :
+#=============================================================================
+#!/user/bin/env python
+import os
+import sys
+
+if __name__ == "__main__":
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "oj.settings")
+
+    from django.core.management import execute_from_command_line
+
+    execute_from_command_line(sys.argv)
