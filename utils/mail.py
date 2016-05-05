4d2d265446fa2c1f661062975071d3538ee80b54
mcmdhr <623742750@qq.com>
2016-05-05 12:49:22 +0800

    Fix class name bug.

--- utils/mail.py
+++ utils/mail.py
@@ -7,16 +7,17 @@
 # Filename     : mail.py
 # Description :
 #=============================================================================
-from envelops import Envelop
+from envelopes import Envelope
 
 from django.conf import settings
 
+
 def send_email(from_name, to_email, to_name, subject, content):
-    envelop = Envelop(from_addr=(settings.SMTP_CONFIG["email"], from_name),
-                      to_addr=(to_email, to_name),
-                      subject=subject,
-                      html_body=content)
-    envelop.send(settings.SMTP_CONFIG["smtp_server"],
-                 login=settings.SMTP_CONFIG["email"],
-                 password=settings.SMTP_CONFIG["password"],
-                 tls=settings.SMTP_CONFIG["tls"])
+    envelope = Envelope(from_addr=(settings.SMTP_CONFIG["email"], from_name),
+                        to_addr=(to_email, to_name),
+                        subject=subject,
+                        html_body=content)
+    envelope.send(settings.SMTP_CONFIG["smtp_server"],
+                  login=settings.SMTP_CONFIG["email"],
+                  password=settings.SMTP_CONFIG["password"],
+                  tls=settings.SMTP_CONFIG["tls"])
